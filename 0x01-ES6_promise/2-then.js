1c642658660d81a5bf59731739606fca058645e9
CzarProCoder <juliusczar36@gmail.com>
2023-10-01 21:31:42 +0300

    Add a function to handle a promise

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,7 +1,5 @@
 export default function handleResponseFromAPI(promise) {
-  return new Promise((resolve, reject) => {
-    if (Promise.resolve()) resolve({status: 200, body: success});
-    else reject(Error());
-    console.log('Got a response from the API');
-  });
+  return promise.then(() => ({ status: 200, body: success }))
+    .catch(() => Error())
+    .finally(() => { console.log('Got a response from the API '); });
 }
