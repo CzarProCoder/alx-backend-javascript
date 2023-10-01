3d065a189dff8ff0020d7de891248db11ae82fb2
CzarProCoder <juliusczar36@gmail.com>
2023-10-01 22:14:49 +0300

    Create a function to return a resolved promise

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,2 +1,12 @@
-import uploadPhoto from "utils.js";
-import createUser from "utils.js";
+/* eslint-disable import/extensions */
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  const upload = uploadPhoto();
+  const create = createUser();
+  return Promise.all([upload, create]).then((result) => {
+    console.log(`${result[0].body} ${result[1].firstName} ${result[1].lastName}`);
+  }).catch(() => {
+    console.log('Signup system offline');
+  });
+}
