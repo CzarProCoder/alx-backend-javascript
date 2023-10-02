590ce4e112834fc3d2b752581015474c170e128c
CzarProCoder <juliusczar36@gmail.com>
2023-10-02 22:37:43 +0300

    Create a class with a default return string structure

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,11 @@
+export default class Airport {
+  constructor(name, code) {
+    if (typeof name !== 'string' || typeof code !== 'string') { throw new Error(); }
+    this._name = name;
+    this._code = code;
+  }
+
+  get [Symbol.toStringTag]() {
+    return this.code;
+  }
+}
