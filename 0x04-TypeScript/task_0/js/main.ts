a343d610c902b0512cfb35084b26cccaf89040c9
CzarProCoder <juliusczar36@gmail.com>
2023-10-04 21:44:03 +0300

    Render a table in TypeScript

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,21 @@
+interface Student {
+  firstName: string;
+  lastName: string;
+  age: number;
+  location: string;
+}
+
+const [student1, student2] = [
+    {
+        firstName: "Julius",
+        lastName: "Kennedy",
+        age: 25,
+        location: "Nairobi"
+    },
+    {
+        firstName: "Lorian",
+        lastName: "James",
+        age: 22,
+        location: "Texas"
+    }
+]
