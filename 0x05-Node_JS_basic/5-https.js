321fc9ced64b518b9f7c4cfec6f781669390b7e7
CzarProCoder <juliusczar36@gmail.com>
2023-11-25 22:08:39 +0300

    Add a server that listens to 2 paths

---
+++ 0x05-Node_JS_basic/5-https.js
@@ -0,0 +1,32 @@
+const http = require('http');
+const countStudents = require('./3-read_file_async');
+
+const dbPath = process.argv[2];
+const hostname = 'localhost';
+const port = 1245;
+
+const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  if (req.url === '/') {
+    res.write('Hello Holberton School!');
+    res.end();
+  } else if (req.url === '/students') {
+    res.write('This is the list of our students');
+    countStudents(dbPath)
+      .then((output) => {
+        res.end(output);
+      })
+      .catch((error) => {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end(error.message);
+      });
+  } else {
+    res.writeHead(404, { 'content-type': 'text/plain' });
+    res.end('Not Found\n');
+  }
+});
+
+app.listen(port, hostname);
+
+module.exports = app;
