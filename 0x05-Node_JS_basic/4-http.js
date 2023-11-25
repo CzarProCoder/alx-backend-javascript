6bf675c6474932631ad82f7b89734d4e6c139c6d
CzarProCoder <juliusczar36@gmail.com>
2023-11-25 13:53:20 +0300

    Create a simple server based on the node http module

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,11 +1,13 @@
 const http = require('http');
 
-const app = http
-  .createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    req.end('Hello Holberton School!');
-  })
-  .listen(1245);
+const port = 1245;
+
+const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Holberton School!');
+});
+
+app.listen(port);
 
 module.exports = app;
