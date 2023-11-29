428d56e65abb543dbdb76758cd3e591a58d1bee8
CzarProCoder <juliusczar36@gmail.com>
2023-11-29 17:09:44 +0300

    Add a module to test payments with tests with stubs

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -3,7 +3,7 @@ const { it, describe } = require('mocha');
 const sinon = require('sinon');
 
 const Utils = require('./utils.js');
-const sendPaymentRequestToApi = require('./3-payment.js');
+const sendPaymentRequestToApi = require('./4-payment.js');
 
 describe('', () => {
   const checkSoy = sinon.spy(console, 'log');
